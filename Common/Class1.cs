0ab252a56e7fe2833521f7338f50627d08f4050d
Danilo Djokic <danilo.djokic@fincore.com>
2025-12-01 11:11:37 +0100

    Day 1 part 1

---
+++ Common/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Common;
+
+public static class Utils
+{
+    public static string[] ReadLines(string fileName)
+    {
+        string projectRoot = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
+        string filePath = Path.Combine(projectRoot, fileName);
+        return File.ReadAllLines(filePath);
+    }
+}
