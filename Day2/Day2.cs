f39eb01f5105e4c63214fa2a9b040ec6b293975f
Danilo Djokic <danilo.djokic@fincore.com>
2025-12-02 12:55:11 +0100

    Day 2 part 2

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -29,7 +29,6 @@ public class Day2(ITestOutputHelper testOutputHelper)
                     long pairSize = digitCount / 2;
                     
                     long divisor = (int)Math.Pow(10, digitCount - pairSize);
-                    long modulo = (int)Math.Pow(10, pairSize);
 
                     var firstN = i / divisor;
                     var secondN = i % divisor;
@@ -44,6 +43,7 @@ public class Day2(ITestOutputHelper testOutputHelper)
 
         return total;
     }
+
     
     [Fact]
     public void Part1_TestInput()
@@ -58,6 +58,77 @@ public class Day2(ITestOutputHelper testOutputHelper)
     {
         var lines = Utils.ReadLines("input.txt");
         long solution = Part1Solution(lines);
+        testOutputHelper.WriteLine(solution.ToString());
+    }
+
+
+    private long Part2Solution(string[] lines)
+    {
+        
+        long total = 0;
+        
+        foreach (var line in lines)
+        {
+            var ranges = line.Split(',');
+            foreach (var range in ranges)
+            {
+                var rangeElements = range.Split('-');
+                var start = long.Parse(rangeElements[0]);
+                var end = long.Parse(rangeElements[1]);
+
+                for (long i = start; i <= end; i++)
+                {
+                    int digitCount = Utils.CountDigits(i);
+                    int maxRepeatablePair = digitCount / 2;
+
+                    for (long j = 1; j <= maxRepeatablePair; j++)
+                    {
+                        int divisor = (int)Math.Pow(10, j);
+                        int digitCollection = (int)(i % divisor);
+                        if (DigitCollectionsAreTheSame(i, digitCollection))
+                        {
+                            total += i;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return total;
+    }
+    
+    private bool DigitCollectionsAreTheSame(long number, int digitCollection)
+    {
+        int digitCollectionCount = Utils.CountDigits(digitCollection);
+        int divisor = (int)Math.Pow(10, digitCollectionCount);
+
+        int firstN = (int)(number % divisor);
+
+        while (number > 0)
+        {
+            if (number % divisor != firstN)
+                return false;
+            
+            number /= divisor;
+        }
+
+        return true;
+    }
+
+    [Fact]
+    public void Part2_TestInput()
+    {
+        var lines = Utils.ReadLines("test.txt");
+        long solution = Part2Solution(lines);
+        Assert.Equal(4174379265, solution);
+    }
+
+    [Fact]
+    public void Part2_RealInput()
+    {
+        var lines = Utils.ReadLines("input.txt");
+        long solution = Part2Solution(lines);
         testOutputHelper.WriteLine(solution.ToString());
     }
 }
