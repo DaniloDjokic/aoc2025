71a6436698dcdff3ab67d1424a15df27f1ec3f46
Danilo Djokic <danilo.djokic@fincore.com>
2025-12-02 11:59:40 +0100

    Day 2 part 1

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -8,4 +8,14 @@ public static class Utils
         string filePath = Path.Combine(projectRoot, fileName);
         return File.ReadAllLines(filePath);
     }
+
+    public static int CountDigits(long num)
+    {
+        return num == 0 ? 1 : (int)Math.Floor(Math.Log10(Math.Abs(num))) + 1;
+    }
+    
+    public static int CountDigits(int num)
+    {
+         return num == 0 ? 1 : (int)Math.Floor(Math.Log10(Math.Abs(num))) + 1;
+    }
 }
---
+++ Day2/Day2.cs
@@ -0,0 +1,63 @@
+﻿using Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Day2;
+
+public class Day2(ITestOutputHelper testOutputHelper)
+{
+    private long Part1Solution(string[] lines)
+    {
+        long total = 0;
+        
+        foreach (var line in lines)
+        {
+            var ranges = line.Split(',');
+            foreach (var range in ranges)
+            {
+                var rangeElements = range.Split('-');
+                var start = long.Parse(rangeElements[0]);
+                var end = long.Parse(rangeElements[1]);
+
+                for (long i = start; i <= end; i++)
+                {
+                    long digitCount = Utils.CountDigits(i);
+
+                    if (digitCount % 2 != 0)
+                        continue;
+                    
+                    long pairSize = digitCount / 2;
+                    
+                    long divisor = (int)Math.Pow(10, digitCount - pairSize);
+                    long modulo = (int)Math.Pow(10, pairSize);
+
+                    var firstN = i / divisor;
+                    var secondN = i % divisor;
+
+                    if (firstN == secondN)
+                    {
+                        total += i;
+                    }
+                }
+            }
+        }
+
+        return total;
+    }
+    
+    [Fact]
+    public void Part1_TestInput()
+    {
+        var lines = Utils.ReadLines("test.txt");
+        long solution = Part1Solution(lines);
+        Assert.Equal(1227775554, solution);
+    }
+
+    [Fact]
+    public void Part1_RealInput()
+    {
+        var lines = Utils.ReadLines("input.txt");
+        long solution = Part1Solution(lines);
+        testOutputHelper.WriteLine(solution.ToString());
+    }
+}
