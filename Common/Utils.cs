3ad65516d17ac4c04647aae8eb6a329b6050e418
Danilo Djokic <danilo.djokic@fincore.com>
2025-12-04 15:16:13 +0100

    Day4 part1

--- Common/Utils.cs
+++ Common/Utils.cs
@@ -18,4 +18,81 @@ public static class Utils
     {
          return num == 0 ? 1 : (int)Math.Floor(Math.Log10(Math.Abs(num))) + 1;
     }
+
+    public class Point(int x, int y, char value)
+    {
+        private int X { get; init; } = x;
+        private int Y { get; init; } = y;
+        public char Value { get; init; } = value;
+
+        private Point FromPoint(Point point)
+        {
+            return new Point(point.X,  point.Y, point.Value);
+        }
+        
+        public Point? ApplyOffset((int x, int y) offset, Point[,] grid)
+        {
+            (int newX, int newY) newPos = (X + offset.x, Y + offset.y);
+
+            if (IsOutOfBounds(grid, newPos))
+            {
+                return null;
+            }
+            
+            return FromPoint(grid[newPos.newX, newPos.newY]);
+        }
+    }
+
+    public static Point[,] ReadGrid(string fileName)
+    {
+        string projectRoot = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
+        string filePath = Path.Combine(projectRoot, fileName);
+
+        var lines = File.ReadAllLines(filePath);
+
+        Point[,] grid = new Point[lines.Length, lines[0].Length];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            for (int j = 0; j < lines[i].Length; j++)
+            {
+                grid[i, j] = new Point(i, j, lines[i][j]);
+            }
+        }
+
+        return grid;
+    }
+
+    public static bool IsOutOfBounds(Point[,] grid, (int x, int y) offset)
+    {
+        return offset.x < 0 || offset.x >= grid.GetLength(0) || offset.y < 0 || offset.y >= grid.GetLength(1);
+    }
+
+    public static List<Point> GetAllAdjacent(Point[,] grid, Point point)
+    {
+        List<Point> points = [];
+        (int x, int y)[] offsets =
+        [
+            (-1, -1), //Top left
+            (-1, 0), //Top
+            (-1, 1), //Top Right
+            (0, -1), //Left
+            (0, 1), //Right
+            (1, -1), //Bot Left
+            (1, 0), //Bot
+            (1, 1) //Bot Right
+        ];
+
+        foreach (var offset in  offsets)
+        {
+            var newPoint = point.ApplyOffset(offset, grid);
+
+            if (newPoint is not null)
+            {
+                points.Add(newPoint);
+            }
+        }
+
+        return points;
+    }
 }
---
+++ Day4/Day4.cs
@@ -0,0 +1,74 @@
+﻿using System.Numerics;
+using Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Day4;
+public class Day4(ITestOutputHelper testOutputHelper)
+{
+    private int Part1Solution(Utils.Point[,] grid)
+    {
+        int total = 0;
+
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                if (grid[i, j].Value == '@' && CanAccess(grid, i, j))
+                {
+                    total++;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    private bool CanAccess(Utils.Point[,] grid, int row, int col)
+    {
+        var point = grid[row, col];
+        
+        var adjacent = Utils.GetAllAdjacent(grid, point);
+        if (adjacent.Count(x => x.Value == '@') <= 3)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    
+    [Fact]
+    public void Part1_TestInput()
+    {
+        var grid = Utils.ReadGrid("test.txt");
+        var result = Part1Solution(grid);
+        Assert.Equal(13, result);
+    }
+
+    [Fact]
+    public void Part1_RealInput()
+    {
+        var grid = Utils.ReadGrid("input.txt");
+        var result = Part1Solution(grid);
+        testOutputHelper.WriteLine(result.ToString());
+    }
+
+
+    private int Part2Solution(string[] lines)
+    {
+        return 0;
+    }
+    
+    [Fact]
+    public void Part2_TestInput()
+    {
+
+    }
+
+    [Fact]
+    public void Part2_RealInput()
+    {
+
+    }
+}
