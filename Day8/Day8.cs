a8a7ccb19a0b04a9c50d16c80bfeedc30523b20b
Danilo Djokic <danilo.djokic@fincore.com>
2025-12-10 14:30:17 +0100

    Day 8 part 2

--- Day8/Day8.cs
+++ Day8/Day8.cs
@@ -13,8 +13,6 @@ public class Day8(ITestOutputHelper testOutputHelper)
         {
             return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2) + Math.Pow(Z - other.Z, 2));
         }
-
-        public List<JunctionBox> Circuit = [];
     }
 
     record JunctionDistance(JunctionBox First, JunctionBox Second, double Distance);
@@ -44,9 +42,6 @@ public class Day8(ITestOutputHelper testOutputHelper)
         List<JunctionBox> processed = [];
         foreach (var distance in distances.Take(connections))
         {
-            distance.First.Circuit.Add(distance.Second);
-            distance.Second.Circuit.Add(distance.First);
-            
             if (!processed.Contains(distance.First))
             {
                 processed.Add(distance.First);
@@ -87,14 +82,6 @@ public class Day8(ITestOutputHelper testOutputHelper)
         {
             circuit.Add(box);
         }
-
-        foreach (var attachedBox in box.Circuit)
-        {
-            if (!circuit.Contains(attachedBox))
-            {
-                NormalizeCircuit(attachedBox, circuit);
-            }
-        }
         
         return circuit;
     }
@@ -133,6 +120,103 @@ public class Day8(ITestOutputHelper testOutputHelper)
     {
         var lines = Utils.ReadLines("input.txt");
         var result = Part1Solution(lines, 1000);
+        testOutputHelper.WriteLine(result.ToString());
+    }
+    
+    private long Part2Solution(string[] lines)
+    {
+        List<JunctionBox> boxes = [];
+
+        foreach (var line in lines)
+        {
+            var vals = line.Split(',');
+            var box = new JunctionBox(int.Parse(vals[0]), int.Parse(vals[1]), int.Parse(vals[2]));
+            boxes.Add(box);
+        }
+
+        List<JunctionDistance> distances = [];
+
+        for (int i = 0; i < boxes.Count; i++)
+        {
+            for (int j = i + 1; j < boxes.Count; j++)
+            {
+                distances.Add(new JunctionDistance(boxes[i], boxes[j], boxes[i].DistanceTo(boxes[j])));
+            }
+        }
+        
+        distances = distances.OrderBy(x => x.Distance).ToList();
+
+        JunctionBox lastFirst = null;
+        JunctionBox lastSecond = null;
+        
+        List<List<JunctionBox>> finalCircuits = [];
+        foreach (var dist in distances)
+        {
+            var first = dist.First;
+            var second = dist.Second;
+            
+            if (boxes.Contains(first))
+                boxes.Remove(first);
+            if (boxes.Contains(second))
+                boxes.Remove(second);
+            
+            finalCircuits.Add([first, second]);
+            finalCircuits = NormalizeCircuits(finalCircuits);
+
+            if (boxes.Count == 0 && finalCircuits.Count == 1)
+            {
+                lastFirst = first;
+                lastSecond = second;
+                break;
+            }
+        }
+
+        return lastFirst!.X * lastSecond!.X;
+    }
+
+    private List<List<JunctionBox>> NormalizeCircuits(List<List<JunctionBox>> circuits)
+    {
+        List<HashSet<JunctionBox>> groups = [];
+        
+        foreach (var circuit in circuits)
+        {
+            var listSet = new HashSet<JunctionBox>(circuit);
+            var matchingGroups = groups.Where(g => g.Intersect(listSet).Any()).ToList();
+
+            if (matchingGroups.Any())
+            {
+                var mergedSet = new HashSet<JunctionBox>(listSet);
+
+                foreach (var group in matchingGroups)
+                {
+                    mergedSet.UnionWith(group);
+                    groups.Remove(group);
+                }
+                
+                groups.Add(mergedSet);
+            }
+            else
+            {
+                groups.Add(listSet);
+            }
+        }
+        
+        return groups.Select(g => g.ToList()).ToList();
+    }
+    
+    [Fact]
+    public void Part2_TestInput()
+    {
+        var lines = Utils.ReadLines("test.txt");
+        var result = Part2Solution(lines);
+        Assert.Equal(25272, result);
+    }
+
+    [Fact]
+    public void Part2_RealInput()
+    {
+        var lines = Utils.ReadLines("input.txt");
+        var result = Part2Solution(lines);
         testOutputHelper.WriteLine(result.ToString());
     }
 }
